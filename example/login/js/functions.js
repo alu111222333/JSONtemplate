f8ec867a478d7cc0d3bce3aefa7cb2212e09d2be
Леонид Арефьев <arefevleonid@gmail.com>
2020-05-23 15:59:34 +0200

    examples

--- example/login/js/functions.js
+++ example/login/js/functions.js
@@ -20,3 +20,11 @@ function getCookie(cname) {
     }
     return "";
 }
+
+function isGoodResponse(json) {
+    if (json.error !== undefined && json.error.state !== undefined && json.error.state) {
+        alert(json.error.title + "\n" + json.error.message); // replace to your own implementation
+        return false;
+    }
+    return true;
+}
